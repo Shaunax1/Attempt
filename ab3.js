20a03014755b800aea0e626950b9d5b96f602016
Shaunax1 <120845915+Shaunax1@users.noreply.github.com>
2022-12-19 21:55:25 +0300

    Add files via upload

---
+++ ab3.js
@@ -0,0 +1,129 @@
+    
+    // Girilen taban uzunluğunu al
+   // var base = prompt("Lütfen üçgenin taban uzunluğunu girin:");
+
+// Üçgeni oluşturmak için döngü oluştur
+
+/* for (var i = 0; i < base; i++) {
+   //Her satır için boşluklar oluştur
+  for (var j = 0; j < base - i; j++) {
+    document.write("&nbsp;");
+  }
+
+  // Her satır için karakterleri yazdır
+  for (var k = 0; k <= i; k++) {
+    document.write("* ");
+  }
+
+  // Her satırın sonuna alt satıra geç
+  document.write("<br>");
+} */
+
+/* ----------------------------------------------- 
+function tablo() {
+  // Tablo oluştur
+  var table = document.createElement("table");
+
+  // Satır oluştur
+  var tr = document.createElement("tr");
+  table.appendChild(tr);
+
+  // İlk hücre oluştur
+  var td1 = document.createElement("td");
+  td1.width = "200";
+  tr.appendChild(td1);
+
+  // İkinci hücre oluştur
+  var td2 = document.createElement("td");
+  td2.width = "200";
+  tr.appendChild(td2);
+
+  // İlk resim oluştur
+  var img1 = document.createElement("img");
+  img1.src = "resim1.JPG";
+  img1.width = "200";
+  td1.appendChild(img1);
+
+  // İkinci resim oluştur
+  var img2 = document.createElement("img");
+  img2.src = "resim2.JPG";
+  img2.width = "200";
+  td2.appendChild(img2);
+}
+
+ ------------------------------------------------------- */
+
+ /* Asal Sayi olup olmadığı */ 
+      /*
+        let girilenSayi;
+        let asalMi = true;
+ 
+        do{
+            girilenSayi = prompt("Lütfen bir sayı giriniz.");
+        } 
+        while(girilenSayi < 2)
+ 
+        for(let i = 2; i < girilenSayi; i++){
+            if(girilenSayi%i == 0){
+                asalMi = false;
+                break;
+            }
+        }
+ 
+        if(asalMi) {
+            alert(girilenSayi + " asal sayıdır!")
+        } else{
+            alert(girilenSayi + " asal sayı değildir!")
+        }
+        */
+/*----------------------------------------------------- */ 
+
+/* ---------------------------------------------------- */ 
+        /* Hesap Makinesi */ 
+
+      /*  
+        document.write("Toplama ");
+        document.write("Çıkartma ");
+        document.write("Çarpma ");
+        document.write("Bölme ");
+        document.write("Sonuc : ");
+        var secim=prompt("Hangi Operatörü Kullanacaksın");
+
+    switch(secim){
+case"1":
+{
+var sayi1=prompt("1. Sayıyı Giriniz");
+var sayi2=prompt("2. Sayıyı Giriniz");
+sonuc=Number(sayi1)+Number(sayi2);
+document.write("Sayıların Toplamı :"+sonuc);
+break;
+}
+case"2":
+{
+var sayi1=prompt("1. Sayıyı Giriniz");
+var sayi2=prompt("2. Sayıyı Giriniz");
+sonuc=Number(sayi1)-Number(sayi2);
+document.write("Sayıların Farkı :"+sonuc);
+break;
+}
+case"3":
+{
+var sayi1=prompt("1. Sayıyı Giriniz");
+var sayi2=prompt("2. Sayıyı Giriniz");
+sonuc=Number(sayi1)*Number(sayi2);
+document.write("Sayıların Çarpımı :"+sonuc);
+break;
+}
+case"4":
+{
+var sayi1=prompt("1. Sayıyı Giriniz");
+var sayi2=prompt("2. Sayıyı Giriniz");
+sonuc=Number(sayi1)/Number(sayi2);
+document.write("Sayıların Bölümü :"+sonuc);
+break;
+}
+
+    }
+    
+    */
+/* ------------------------------------------------------------- */ 
